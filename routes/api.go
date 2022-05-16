fdf973eed793e9fa165f97f9f17af39621302b29
b-waynebai <yue11037417@gmail.com>
2022-05-16 10:59:20 +0800

    手机号是否注册

--- routes/api.go
+++ routes/api.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,12 +11,10 @@ func RegisterApiRoutes(r *gin.Engine) {
 	// 测试一个v1的路由
 	v1 := r.Group("/v1")
 	{
-		// 注册一个路由
-		v1.GET("/", func(ctx *gin.Context) {
-			// 以JSON格式响应
-			ctx.JSON(http.StatusOK, gin.H{
-				"Hello": "World!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
